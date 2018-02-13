994241dfdcd6f5deea2f2eb883844c9e5fded262
Loris Friedel <loris.friedel@gmail.com>
2018-02-13 19:33:04 +0100

    Auto stash before merge of "master" and "origin/master"

--- run.sh
+++ run.sh
@@ -13,17 +13,16 @@ fi
 if [ ! -f $FILE ]; then
     if [ ! -f "src/main/resources/$FILE.ssl" ]; then
         mvn exec:java -Dexec.args="src/main/resources/$FILE.ssl"
+    else
+        echo "file '$FILE' not found"
+        exit 1
     fi
-
-    echo "file '$FILE' not found"
-    exit 1
+else
+    mvn exec:java -Dexec.args="$FILE"
 fi
 
-mvn exec:java -Dexec.args="$FILE"
-
-# TODO build every docker images
 for i in $(find ./generated -mindepth 1 -maxdepth 1 -type d)
 do
-chmod +x $i/build.sh
-$i/build.sh $2 $(basename $i)
+    chmod +x $i/build.sh
+    $i/build.sh $2 $(basename $i)
 done
