2906bbb378bd4088e483747897e6e053326fcd30
cesar colle <cesar.colle@gmail.com>
2018-02-13 12:01:33 +0100

    Add randomLaw generator for different type

--- src/main/java/hcs/dsl/ssl/generator/SSLAppGenerator.java
+++ src/main/java/hcs/dsl/ssl/generator/SSLAppGenerator.java
@@ -7,10 +7,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
-import hcs.dsl.ssl.backend.law.Law;
-import hcs.dsl.ssl.backend.law.MarkovLaw;
-import hcs.dsl.ssl.backend.law.RandomLaw;
+import hcs.dsl.ssl.backend.law.*;
 import hcs.dsl.ssl.backend.misc.Interval;
+import hcs.dsl.ssl.backend.misc.ListWrapper;
+import hcs.dsl.ssl.backend.misc.Var;
 import hcs.dsl.ssl.backend.sensor.Sensor;
 
 import hcs.dsl.ssl.runtime.law.random.RandomLawIntervalInteger;
@@ -47,6 +47,11 @@ public class SSLAppGenerator implements Runnable {
         context.put("sensors", model.sensors);
         context.put("global", model.global);
 
+        context.put("RandomLaw", RandomLaw.class);
+        context.put("MarkovLaw", MarkovLaw.class);
+        context.put("FunctionLaw", FunctionLaw.class);
+
+
     }
 
     public void writeTemplate(){
@@ -72,14 +77,53 @@ public class SSLAppGenerator implements Runnable {
 
         Map<String, Law> laws = new HashMap<>();
         Map<String, Sensor> sensors = new HashMap<>();
+        // MARKOV
+        MarkovLaw markovLaw = new MarkovLaw("MarkovString");
+        List<Edge> edges = Arrays.asList(
+                new Edge<>("lol", 0.1, "lolilol"),
+                new Edge<>("lolilol", 0.2, "lol"));
+        markovLaw.setList(edges);
+        markovLaw.setValType(Var.Type.String);
 
+        MarkovLaw markovLawInteger = new MarkovLaw("MarkovInteger");
+        List<Edge> ed = Arrays.asList(
+                new Edge<>(1, 0.1, 2),
+                new Edge<>(2, 0.2, 1));
+        markovLawInteger.setList(ed);
+        markovLawInteger.setValType(Var.Type.Integer);
+
+        laws.put("markoov", markovLaw);
+        laws.put("markovInteger", markovLawInteger);
+
+        // FUNCTION
+        FunctionLaw functionLaw = new FunctionLaw("myFunction");
+        functionLaw.setCases(Arrays.asList(new CaseFunc("ici", "la"),
+                new CaseFunc("labas", "where")));
+        laws.put("myFunction", functionLaw);
+
+        // RANDOM
         RandomLaw randomLaw = new RandomLaw("myRandom");
         randomLaw.setInterval(new Interval<>(Interval.Type.Integer, 1, 2));
 
+        RandomLaw randomLawList = new RandomLaw("myRandomList");
+        randomLawList.setList(new ListWrapper(Var.Type.Integer, Arrays.asList(1, 2, 3, 4)));
+
+        RandomLaw randomLawListString = new RandomLaw("myRandomListString");
+        randomLawListString.setList(new ListWrapper(Var.Type.String, Arrays.asList("a", "b", "c", "d")));
+
+
+        laws.put("randomLawListString", randomLawListString);
+        laws.put("randomList", randomLawList);
         laws.put("random", randomLaw);
+
+
+
 
         Sensor sensor = new Sensor("lumi");
         sensors.put("lumi", sensor);
+        sensor.setNoise(new Interval<>(Interval.Type.Integer, 1, 2));
+        sensor.setOffset("10/10/1010 10:11");
+        sensor.setPeriod("10ms");
 
 
         Model model = new Model(laws, sensors, null, null, null);
