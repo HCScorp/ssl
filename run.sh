7c4743b8f02b54d603d30f300b84ef5024b3d852
Thomas Munoz <thomas.munoz30@gmail.com>
2018-02-13 19:27:43 +0100

    Improve run script to build if not already done

--- run.sh
+++ run.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
-mvn clean package -Dmaven.test.skip=true
-mvn exec:java -Dexec.args="src/main/resources/$1.ssl"
+cd $(dirname $0)
+
+FILE=$1
+DOCKER_USER=$2
+
+if [ ! -f ./target/ssl-antlr-1.0-SNAPSHOT.jar ]; then
+    mvn clean package
+fi
+
+if [ ! -f $FILE ]; then
+    if [ ! -f "src/main/resources/$FILE.ssl" ]; then
+        mvn exec:java -Dexec.args="src/main/resources/$FILE.ssl"
+    fi
+
+    echo "file '$FILE' not found"
+fi
+
+mvn exec:java -Dexec.args="$FILE"
 
