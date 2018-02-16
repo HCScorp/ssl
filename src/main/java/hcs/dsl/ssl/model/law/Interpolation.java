20316b5ef7a37902e874f0d3b03cec428688119c
cesar colle <cesar.colle@gmail.com>
2018-02-16 11:14:36 +0100

    Add the template for interpolation

--- src/main/java/hcs/dsl/ssl/model/law/Interpolation.java
+++ src/main/java/hcs/dsl/ssl/model/law/Interpolation.java
@@ -26,5 +26,22 @@ public class Interpolation {
         return restriction;
     }
 
+    public String codeRestriction(){
+        if (restriction != null) {
+             return ",new Restriction<>" + "(" + restriction +")";
+        }
+        return "";
+    }
 
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append(coefPolynome.get(0));
+        for (int i = 1; i < coefPolynome.size(); i++) {
+            result.append("+").append(coefPolynome.get(i)).append("x^").append(i);
+        }
+
+
+        return result.toString();
+    }
 }
