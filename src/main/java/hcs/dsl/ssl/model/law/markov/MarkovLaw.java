70e71bff3f2337587446da5cd0854d4d8fc45946
Thomas Munoz <thomas.munoz30@gmail.com>
2018-02-20 22:38:15 +0100

    Add compile check for proba sum of markov law

--- src/main/java/hcs/dsl/ssl/model/law/markov/MarkovLaw.java
+++ src/main/java/hcs/dsl/ssl/model/law/markov/MarkovLaw.java
@@ -3,7 +3,11 @@ package hcs.dsl.ssl.model.law.markov;
 import hcs.dsl.ssl.model.law.Law;
 import hcs.dsl.ssl.model.misc.ValType;
 
+import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class MarkovLaw extends Law {
 
@@ -28,7 +32,22 @@ public class MarkovLaw extends Law {
     }
 
     public void setList(List<Edge> list) {
+        final Map<Object, TreeMap<Object, Double>> edgeMap = new HashMap<>();
+        for(Edge e : list) {
+            if (!edgeMap.containsKey(e.getFrom())) {
+                edgeMap.put(e.getFrom(), new TreeMap<>());
+            }
+
+            edgeMap.get(e.getFrom()).put(e.getTo(), e.getProba());
+        }
+
+        edgeMap.forEach((o, e) -> {
+            Double totalWeight = e.entrySet().stream().mapToDouble(Map.Entry::getValue).sum();
+            if (totalWeight != 1.0) {
+                throw new IllegalArgumentException("total weight is " + totalWeight + " but must be between 1.0 (for value: " + o + ") of law " + getName());
+            }
+        });
+
         this.list = list;
-        // TODO check proba sum
     }
 }
