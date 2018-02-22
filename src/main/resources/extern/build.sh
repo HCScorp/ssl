29b2519f835a6c701c284a88a76c28065e4a0283
Thomas Munoz <thomas.munoz30@gmail.com>
2018-02-22 21:32:26 +0100

    Fix bad substitution in build script

--- src/main/resources/extern/build.sh
+++ src/main/resources/extern/build.sh
@@ -6,7 +6,7 @@ mvn clean formatter:format package
 if [ $? -eq 0 ]; then
     echo "Application build succeed!"
 
-    docker build -t $1/${2,,} .
+    docker build -t $1/$(echo -n "$2" | tr '[:upper:]' '[:lower:]') .
     if [ $? -eq 0 ]; then
         echo "Docker image build succeed!"
     else
